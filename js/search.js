ae54b88dfe964cb7e168072b42ae7a9a1b574de2
Brian Walker <bwalker206@gmail.com>
2021-07-22 00:13:24 -0400

    Modify filter search/add responsive grid

--- js/search.js
+++ js/search.js
@@ -1,6 +1,6 @@
 function myFunction() {
   // Declare variables
-  var input, filter, ul, li, a, i, txtValue;
+  var input, filter, ul, li, a, i;
   input = document.getElementById("myInput");
   filter = input.value.toUpperCase();
   ul = document.getElementById("myUL");
@@ -9,11 +9,17 @@ function myFunction() {
   // Loop through all list items, and hide those who don't match the search query
   for (i = 0; i < li.length; i++) {
     a = li[i].getElementsByTagName("a")[0];
-    txtValue = a.textContent || a.innerText;
-    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+    if (a.getAttribute("data-caption").toUpperCase().indexOf(filter) > -1) {
       li[i].style.display = "";
     } else {
       li[i].style.display = "none";
     }
   }
+
+  // If the search input is empty, display all list items
+  if (input.value === "") {
+    ul.style.display = "";
+  }
 }
+
+myFunction();
